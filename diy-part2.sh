4077c280ce7d49dfb5087a27996de61d24309439
ngws1111 <146655237+ngws1111@users.noreply.github.com>
2026-02-12 20:44:24 +0800

    Comment out default login address modification
    Comment out the line that changes the default login address.

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # 修改openwrt默认登陆地址192.168.1.1改成192.168.18.1
-sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
 #设置登录密码为空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
